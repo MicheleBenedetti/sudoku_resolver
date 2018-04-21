28d658405ca6686e6ceab4e109fcb73fe81b203f
Michele Benedetti <elmiki_2000@hotmail.it>
2018-04-21 08:52:54 +0200

    Add files via upload

--- slittamento.js
+++ slittamento.js
@@ -5,23 +5,23 @@ slittamento = (sudoku) => {
         for (let num of riga){
             if (num in riga.slice(0,3)){
                 if (num in sudoku[riga + 1].slice(3,6) || num in sudoku[riga + 2].slice(3,6)){
-                    possible_coloumns.add(6,7,8);
+                    possible_coloumns[num] = [6,7,8];
                 }else if (num in sudoku[riga + 1].slice(6,9) || num in sudoku[riga + 2].slice(6,9)){
-                    possible_coloumns.add(3,4,5);
+                    possible_coloumns[num] = [3,4,5];
                 }
             }
             else if (num in riga.slice(3,6)){
                 if (num in sudoku[riga + 1].slice(0,3) || num in sudoku[riga + 2].slice(0,3)){
-                    possible_coloumns.add(6,7,8);
+                    possible_coloumns[num] = [6,7,8];
                 }else if (num in sudoku[riga + 1].slice(6,9) || num in sudoku[riga + 2].slice(6,9)){
-                    possible_coloumns.add(0,1,2);
+                    possible_coloumns[num] = [0,1,2];
                 }
             }
             else if (num in riga.slice(6,9)){
                 if (num in sudoku[riga + 1].slice(0,3) || num in sudoku[riga + 2].slice(0,3)){
-                    possible_coloumns.add(3,4,5);
+                    possible_coloumns[num] = [3,4,5];
                 }else if (num in sudoku[riga + 1].slice(3,6) || num in sudoku[riga + 2].slice(3,6)){
-                    possible_coloumns.add(0,1,2);
+                    possible_coloumns[num] = [0,1,2];
                 }
             }
         }
